e96d8c1edd746d0e3d6d1297d159dfe3c90af16b
hyeonhee <nloavee@gmail.com>
2025-02-04 11:11:10 +0900

    관심 상품 등록 API

---
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
