3606e14501e3a32a53a355f843f5e9a62f10e530
hyeonhee <nloavee@gmail.com>
2025-02-04 11:18:36 +0900

    관심상품 희망 최저가 API

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 }
