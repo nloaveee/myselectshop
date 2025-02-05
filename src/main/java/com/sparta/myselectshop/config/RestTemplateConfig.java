5bef40eb1566b407b2248fa8f3e21067838d06f2
hyeonhee <nloavee@gmail.com>
2025-02-05 11:29:48 +0900

    인가 코드 access_token 가져오기 & 카카오 사용자 정보 가져오기

---
+++ src/main/java/com/sparta/myselectshop/config/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.sparta.myselectshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig  {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000); // 연결 타임아웃 (ms 단위)
+        factory.setReadTimeout(5000); // 응답 타임아웃 (ms 단위)
+        return new RestTemplate(factory);
+    }
+
+}
